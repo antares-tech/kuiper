7ac9073de8ca5ff39b0bb3b0622b9d7b0f75411d
Soumik Pradhan <soumik@antarx.tech>
2019-05-03 13:15:15 +0530

    Remove services, apps and their tests

--- apps/auth/common/user.js
+++
@@ -1,8 +0,0 @@
-var user = {
-	username : "akshat",
-	password : "akshat",
-	email    : "akshat2820@gmail.com",
-};
-
-module.exports = user;
-
--- apps/auth/routes/social-signon.js
+++
@@ -1,8 +0,0 @@
-var express       = require('express');
-var router        = express.Router();
-var log           = require('common/lib/log');
-var social_google = require('auth/routes/social/google');
-
-router.use('/google', social_google);
-
-module.exports = router;
