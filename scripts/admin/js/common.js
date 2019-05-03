ab7399476e1df91b1be9641c328ecbdba78c7f05
Soumik Pradhan <soumik@antarx.tech>
2019-05-04 00:13:51 +0530

    cleanup scripts

--- scripts/admin/js/common.js
+++
@@ -1,9 +0,0 @@
-var common = {};
-
-common.print_args = () => {
-	console.log ('    Common arguments :');
-	console.log ('        --help                   : print this help');
-	console.log ('        --instance               : specify the service instance #');
-};
-
-module.exports = common;
